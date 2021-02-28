7c7960006130d0f06ec80b57064d73b54ba37dcd
Zayer Lwin <zyl.pol.mm.81298@outlook.com>
2021-02-28 12:59:38 +0630

    0.0.4

--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@
 int main(){
 	double h;
 	std::cout << "Enter geometric altitude";
-	while(true)
+	try{
+		while(true)
+		{
+			std::cin >> h;
+			std::cout << std::setw(12) << h << std::setw(12) << geopotential_height(h)
+				<< std::setw(12) << isa_temperature(h) << std::setw(12) << isa_pressure(h)
+				<< std::setw(12) << isa_density(h) << std::endl; 
+		}
+	} catch ( std::runtime_error &e)
 	{
-		std::cin >> h;
-		std::cout << std::setw(12) << h << std::setw(12) << geopotential_height(h)
-			<< std::setw(12) << isa_temperature(h) << std::setw(12) << isa_pressure(h)
-			<< std::setw(12) << isa_density(h) << std::endl; 
+		std::cerr << "Runtime error: " << e.what() << std::endl;
+		return 1;
 	}
 	return 0;
 }
