a724f016da247a07a748af7de2b749877349f19e
Zayer Lwin <zyl.pol.mm.81298@outlook.com>
2021-03-01 12:06:47 +0630

    0.1.0

--- main.cpp
+++ main.cpp
@@ -2,22 +2,48 @@
 #include "isa.hpp"
 #include <iomanip>
 #include <fstream>
+#include <string>
 
+void draw_table();
 int main(){
-	double h;
-	std::cout << "Enter geometric altitude";
-	try{
-		while(true)
-		{
-			std::cin >> h;
-			std::cout << std::setw(12) << h << std::setw(12) << geopotential_height(h)
-				<< std::setw(12) << isa_temperature(h) << std::setw(12) << isa_pressure(h)
-				<< std::setw(12) << isa_density(h) << std::endl; 
-		}
-	} catch ( std::runtime_error &e)
-	{
-		std::cerr << "Runtime error: " << e.what() << std::endl;
-		return 1;
-	}
+	draw_table();
 	return 0;
+}
+
+void draw_table(){
+	std::string filename;
+	std::cout << "Enter filename to draw table ";
+	std::cin >> filename;
+	std::ofstream out{filename};
+
+	if(!out)
+	{
+		std::cerr << "can't open file: " << filename << std::endl;
+		exit(1);
+	}
+
+	double end_height;
+	double interval;
+
+	std::cout << "Enter table end geometric height ";
+	std::cin >> end_height;
+	std::cout << "Enter interval between two height ";
+	std::cin >> interval;
+	char seperator = ',';
+	out << "Geometric height" << seperator << "Geopotential height" << seperator << "temperature"
+		<< seperator << "pressure" << seperator << "density" << std::endl;
+
+	double height = 0;
+	while(true)
+	{
+		out << height << seperator << geopotential_height(height) << seperator << isa_temperature(height) << seperator
+			<< isa_pressure(height) << seperator << isa_density(height) << std::endl;
+		height += interval;
+		if(static_cast<int>(height) == static_cast<int>(end_height))
+		{
+			break;
+		} 
+	}
+
+	out << std::setw(40) <<  "END" << std::endl;
 }
